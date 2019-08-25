9c7f061e1501249e311cda089459a7c96cfb3f70
Guillaume Sanchez <gsanchez@hexaglobe.com>
2019-08-25 11:42:51 -0400

    format

--- torchelie/transforms/differentiable.py
+++ torchelie/transforms/differentiable.py
@@ -4,18 +4,21 @@ import numpy as np
 
 # FIXME: The API is not that great, improve it.
 
+
 def _rollx(img, begin, to):
     if to <= img.shape[1]:
         return img[:, :, begin:to]
     else:
-        return torch.cat([img[:, :, begin:], img[:, :, :to - img.shape[2]]], dim=2)
+        return torch.cat([img[:, :, begin:], img[:, :, :to - img.shape[2]]],
+                         dim=2)
 
 
 def _rolly(img, begin, to):
     if to <= img.shape[0]:
         return img[:, begin:to, :]
     else:
-        return torch.cat([img[:, begin:, :], img[:, :to - img.shape[1], :]], dim=1)
+        return torch.cat([img[:, begin:, :], img[:, :to - img.shape[1], :]],
+                         dim=1)
 
 
 def crop(batch_img, warped=True, sub_img_factor=2):
@@ -32,53 +35,46 @@ def crop(batch_img, warped=True, sub_img_factor=2):
         else:
             y = np.random.randint(sz[1] - h)
             x = np.random.randint(sz[2] - w)
-            img_crop = img[:, y:y+h, x:x+w]
+            img_crop = img[:, y:y + h, x:x + w]
         imgs.append(img_crop)
     return torch.stack(imgs)
 
 
 def _gblur_kernel_2d(c):
-    gaussian = np.array([
-        [1, 2, 1],
-        [2, 4, 2],
-        [1, 2, 1]
-    ]) / 16
+    gaussian = np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]]) / 16
     kernel = np.zeros((3, 3, 3))
     kernel[c, :, :] = gaussian
     return kernel
+
 
 def _gblur_kernel():
-    return np.stack([
-        _gblur_kernel_2d(0),
-        _gblur_kernel_2d(1),
-        _gblur_kernel_2d(2)
-    ]).astype(float)
+    return np.stack(
+        [_gblur_kernel_2d(0),
+         _gblur_kernel_2d(1),
+         _gblur_kernel_2d(2)]).astype(float)
+
 
 def gblur(input):
-    return F.conv2d(
-            input,
-            torch.FloatTensor(_gblur_kernel()).cuda(),
-            padding=1)
+    return F.conv2d(input,
+                    torch.FloatTensor(_gblur_kernel()).cuda(),
+                    padding=1)
+
 
 def _mblur_kernel_2d(c):
-    gaussian = np.array([
-        [1, 1, 1],
-        [1, 1, 1],
-        [1, 1, 1]
-    ]) / 8
+    gaussian = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]) / 8
     kernel = np.zeros((3, 3, 3))
     kernel[c, :, :] = gaussian
     return kernel
 
+
 def _mblur_kernel():
-    return np.stack([
-        _mblur_kernel_2d(0),
-        _mblur_kernel_2d(1),
-        _mblur_kernel_2d(2)
-    ]).astype(float)
+    return np.stack(
+        [_mblur_kernel_2d(0),
+         _mblur_kernel_2d(1),
+         _mblur_kernel_2d(2)]).astype(float)
+
 
 def mblur(input):
-    return F.conv2d(
-            input,
-            torch.FloatTensor(_mblur_kernel()).cuda(),
-            padding=1)
+    return F.conv2d(input,
+                    torch.FloatTensor(_mblur_kernel()).cuda(),
+                    padding=1)
