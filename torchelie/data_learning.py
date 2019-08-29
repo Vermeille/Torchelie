3fda28e99ecb91ed29382b2a264fe344a82373a1
Guillaume Sanchez <gsanchez@hexaglobe.com>
2019-08-29 19:04:01 -0400

    documentation for data_learning

--- torchelie/data_learning.py
+++ torchelie/data_learning.py
@@ -10,6 +10,17 @@ def _rfft2d_freqs(h, w):
 
 
 class PixelImage(nn.Module):
+    """
+    A learnable image parameterized by its pixel values
+
+    Args:
+        shape (tuple of int): a tuple like (channels, height, width)
+        sd (float): pixels are initialized with a random gaussian value of mean
+            0.5 and standard deviation `sd`
+        init_img (tensor, optional): an image tensor to initialize the pixel
+            values
+    """
+
     def __init__(self, shape, sd=0.01, init_img=None):
         super(PixelImage, self).__init__()
         ch, h, w = shape
@@ -19,10 +30,28 @@ class PixelImage(nn.Module):
             self.pixels.data.copy_(init_img - 0.5)
 
     def forward(self):
-        return (self.pixels)# + 0.5).clamp(0, 1)
+        """
+        Return the image
+        """
+        return self.pixels
 
 
 class SpectralImage(nn.Module):
+    """
+    A learnable image parameterized by its Fourier representation.
+
+    See https://distill.pub/2018/differentiable-parameterizations/
+
+    Implementation ported from
+    https://github.com/tensorflow/lucid/blob/master/lucid/optvis/param/spatial.py
+
+    Args:
+        shape (tuple of int): a tuple like (channels, height, width)
+        sd (float): amplitudes are initialized with a random gaussian value of
+            mean 0.5 and standard deviation `sd`
+        init_img (tensor, optional): an image tensor to initialize the image
+    """
+
     def __init__(self, shape, sd=0.01, decay_power=1, init_img=None):
         super(SpectralImage, self).__init__()
         self.shape = shape
@@ -46,6 +75,9 @@ class SpectralImage(nn.Module):
             self.spectrum_var.data.copy_(fft / spertum_scale)
 
     def forward(self):
+        """
+        Return the image
+        """
         ch, h, w = self.shape
 
         scaled_spectrum = self.spectrum_var * self.spertum_scale
@@ -56,6 +88,11 @@ class SpectralImage(nn.Module):
 
 
 class CorrelateColors(torch.nn.Module):
+    """
+    Takes an learnable image and applies the inverse color decorrelation from
+    ImageNet (ie, it correlates the color like ImageNet to ease optimization)
+    """
+
     def __init__(self):
         super(CorrelateColors, self).__init__()
         color_correlation_svd_sqrt = torch.FloatTensor([[0.26, 0.09, 0.02],
@@ -69,12 +106,18 @@ class CorrelateColors(torch.nn.Module):
                              color_correlation_svd_sqrt / max_norm_svd_sqrt)
 
     def forward(self, t):
+        """
+        Correlate the color of the image `t` and return the result
+        """
         t_flat = t.view(t.shape[0], 3, -1).transpose(2, 1)
         t_flat = torch.matmul(t_flat, self.color_correlation.t())
         t = t_flat.transpose(2, 1).view(t.shape)
         return t
 
     def invert(self, t):
+        """
+        Decorrelate the color of the image `t` and return the result
+        """
         t_flat = t.view(3, -1).transpose(1, 0)
         t_flat = torch.matmul(t_flat, self.color_correlation.inverse().t())
         t = t_flat.transpose(1, 0).view(t.shape)
@@ -82,8 +125,26 @@ class CorrelateColors(torch.nn.Module):
 
 
 class ParameterizedImg(nn.Module):
-    def __init__(self, *shape, init_sd=0.06, init_img=None, space='spectral',
-            colors='uncorr'):
+    """
+    A convenient wrapper around `PixelImage` and `SpectralImage` and
+    `CorrelateColors` to make a learnable image.
+
+    Args:
+        *shape (int): shape of the image: channel, height, width
+        init_sd (float): standard deviation for initializing the image if
+            `init_img` is `None`
+        init_img (tensor): an image to use as initialization
+        space (str): either `"pixel"` or `"spectral"` to have the underlying
+            representation be a `PixelImage` or a `SpectralImage`
+        colors (str): either `"corr"` or `"uncorr"`, to use a correlated or
+            decorrelated color space
+    """
+    def __init__(self,
+                 *shape,
+                 init_sd=0.06,
+                 init_img=None,
+                 space='spectral',
+                 colors='uncorr'):
         super(ParameterizedImg, self).__init__()
 
         if init_img is not None:
@@ -103,10 +164,16 @@ class ParameterizedImg(nn.Module):
         else:
             self.img = PixelImage(shape, sd=init_sd, init_img=init_img)
 
-
     def forward(self):
+        """
+        Return the tensor
+        """
         t = self.corr(self.img())
         return torch.sigmoid(t)
 
     def render(self):
+        """
+        Return the tensor on cpu and detached, ready to be transformed to a PIL
+        image
+        """
         return self.forward().cpu().detach()[0]
