885ba8c85fb61bd08fbda264ad8488b8afcedab3
Guillaume Sanchez <gsanchez@hexaglobe.com>
2019-09-04 11:22:36 -0400

    setup.py fixed

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
         name='Torchelie',
         version='0.1dev',
-        packages=['torchelie',],
+        packages=find_packages(),
         license='Creative Commons Attribution-Noncommercial-Share Alike license',
+        install_requires=[
+            'visdom>=0.1.8',
+            'crayons>=0.2',
+            'torch>=1.2',
+            'torchvision>=0.4',
+            'numpy>=1.16',
+            'Pillow>=6',
+            'caffe2>=0.8',
+            'onnx>=1.5',
+        ]
 )
