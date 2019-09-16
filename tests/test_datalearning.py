151251309f1550475c2959ea25c76fe5fb7a79b0
Guillaume Sanchez <gsanchez@hexaglobe.com>
2019-09-16 07:04:32 -0400

    start regression tests

---
+++ tests/test_datalearning.py
@@ -0,0 +1,58 @@
+import torch
+from torchelie.data_learning import *
+
+
+def test_pixel_image():
+    pi = PixelImage((3, 128, 128), 0.01)
+    pi()
+
+    start = torch.randn(3, 128, 128)
+    pi = PixelImage((3, 128, 128), init_img=start)
+
+    assert start.allclose(pi() + 0.5, atol=1e-7)
+
+
+def test_spectral_image():
+    pi = SpectralImage((3, 128, 128), 0.01)
+    pi()
+
+    start = torch.randn(3, 128, 128)
+    pi = SpectralImage((3, 128, 128), init_img=start)
+
+
+def test_correlate_colors():
+    corr = CorrelateColors()
+    start = torch.randn(1, 3, 64, 64)
+    assert start.allclose(corr.invert(corr(start)), atol=1e-5)
+
+
+def test_parameterized_img():
+    start = torch.clamp(torch.randn(3, 128, 128) + 0.5, min=0, max=1)
+
+    ParameterizedImg(3, 128, 128, space='spectral', colors='uncorr')()
+    ParameterizedImg(3,
+                     128,
+                     128,
+                     space='spectral',
+                     colors='uncorr',
+                     init_img=start)()
+
+    ParameterizedImg(3, 128, 128, space='pixel', colors='uncorr')()
+    ParameterizedImg(3,
+                     128,
+                     128,
+                     space='pixel',
+                     colors='uncorr',
+                     init_img=start)()
+
+    ParameterizedImg(3, 128, 128, space='spectral', colors='corr')()
+    ParameterizedImg(3,
+                     128,
+                     128,
+                     space='spectral',
+                     colors='corr',
+                     init_img=start)()
+
+    ParameterizedImg(3, 128, 128, space='pixel', colors='corr')()
+    ParameterizedImg(3, 128, 128, space='pixel', colors='corr',
+                     init_img=start)()
