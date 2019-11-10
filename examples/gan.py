5698253ea99404910df9ab351ff44e5be9934cf9
Guillaume Sanchez <gsanchez@hexaglobe.com>
2019-11-09 23:00:10 -0500

    update GAN example

--- examples/gan.py
+++ examples/gan.py
@@ -1,94 +1,124 @@
-import sys
 import argparse
-
-import crayons
-
-from visdom import Visdom
+import copy
 
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.optim import Adam
 
-from torchvision.datasets import MNIST, CIFAR10
+from torchvision.datasets import MNIST, CIFAR10, FashionMNIST, CelebA, ImageFolder
 import torchvision.transforms as TF
 
-import torchelie.nn as tnn
-from torchelie.utils import kaiming, xavier
-import torchelie.models
+import torchelie.models as tmodels
 from torchelie.models import VggGeneratorDebug, Patch32, VggDebug
 from torchelie.utils import nb_parameters, freeze, unfreeze
-import torchelie.loss.gan.standard as gan_loss
+from torchelie.optim import RAdamW
+import torchelie.loss.gan.hinge as gan_loss
+from torchelie.datasets import NoexceptDataset
+from torchelie.transforms import AdaptPad
+from torchelie.recipes.gan import GANLoop
+import torchelie.metrics.callbacks as tcb
+from torch.optim import AdamW
+from torchelie.recipes.recipebase import DataLoop
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cpu', action='store_true')
 parser.add_argument('--dataset',
                     type=str,
-                    choices=['mnist', 'cifar10'],
                     default='mnist')
 parser.add_argument('--shapes-only', action='store_true')
 opts = parser.parse_args()
 
 device = 'cpu' if opts.cpu else 'cuda'
+BS = 32
 
-
-vis = Visdom(env='gan_dbg')
-
-tfms = TF.Compose([TF.Resize(32), TF.ToTensor()])
+tfms = TF.Compose([
+    TF.Resize(64),
+    AdaptPad((64, 64)),
+    TF.ToTensor()])
 if opts.dataset == 'mnist':
     ds = MNIST('~/.cache/torch/mnist', download=True, transform=tfms)
-if opts.dataset == 'cifar10':
+elif opts.dataset == 'cifar10':
     ds = CIFAR10('~/.cache/torch/cifar10', download=True, transform=tfms)
+elif opts.dataset == 'fashion':
+    ds = FashionMNIST('~/.cache/torch/fashionmnist/',
+                      download=True,
+                      transform=tfms)
+elif opts.dataset == 'celeba':
+    tfms = TF.Compose([
+        TF.CenterCrop(120),
+        TF.Resize(64),
+        AdaptPad((64, 64)),
+        TF.ToTensor()])
+    ds = CelebA('~/.cache/torch/celeba/', download=True, transform=tfms)
+else:
+    ds = NoexceptDataset(ImageFolder(opts.dataset, transform=tfms))
 dl = torch.utils.data.DataLoader(ds,
                                  num_workers=4,
-                                 batch_size=32,
+                                 batch_size=BS,
                                  shuffle=True)
+
+
 def summary(Net):
     clf = Net(10, in_ch=1, debug=True).to(device)
     clf(torch.randn(32, 1, 32, 32).to(device))
     print('Nb parameters: {}'.format(nb_parameters(clf)))
 
 def train_net(Gen, Discr):
-    G = Gen(in_noise=32, out_ch=1).to(device)
-    D = Discr(in_ch=1, out_ch=1, norm=None).to(device)
+    G = Gen(in_noise=128, out_ch=3)
+    G_polyak = copy.deepcopy(G).eval()
+    D = Discr(in_ch=3, out_ch=1)
 
-    opt_G = Adam(G.parameters(), lr=2e-4, betas=(0., 0.999))
-    opt_D = Adam(D.parameters(), lr=2e-4, betas=(0., 0.999))
+    def G_fun(batch):
+        z = torch.randn(BS, 128, device=device)
+        fake = G(z)
+        preds = D(fake * 2 - 1).squeeze()
+        loss = gan_loss.generated(preds)
+        loss.backward()
+        return {'loss': loss.item(), 'imgs': fake.detach()}
 
-    iters = 0
-    for epoch in range(4):
-        for x, y in dl:
-            x = x.to(device)
-            y = y.to(device)
+    def G_polyak_fun(batch):
+        print('POLYAK')
+        z = torch.randn(BS, 128, device=device)
+        fake = G_polyak(z)
+        return {'imgs': fake.detach()}
 
-            z = torch.randn(16, 32).to(device)
-            fake = G(z)
+    def D_fun(batch):
+        z = torch.randn(BS, 128, device=device)
+        fake = G(z)
+        fake_loss = gan_loss.fake(D(fake * 2 - 1))
+        fake_loss.backward()
 
-            opt_D.zero_grad()
-            loss_fake = gan_loss.fake(D(fake.detach() * 2 - 1))
-            loss_fake.backward()
+        x = batch[0]
+        x = x.expand(-1, 3, -1, -1)
 
-            loss_true = gan_loss.real(D(x * 2 - 1))
-            loss_true.backward()
-            opt_D.step()
+        real_loss = gan_loss.real(D(x * 2 - 1))
+        real_loss.backward()
 
-            opt_G.zero_grad()
-            freeze(D)
-            loss = gan_loss.generated(D(fake * 2 - 1))
-            loss.backward()
-            unfreeze(D)
-            opt_G.step()
+        loss = real_loss.item() + fake_loss.item()
+        return {'loss': loss, 'real_loss': real_loss.item(), 'fake_loss':
+                fake_loss.item()}
 
-            if iters % 100 == 0:
-                print("Iter {}, loss true {}, loss fake {}, loss G {}".format(
-                    iters, loss_true.item(), loss_fake.item(), loss.item()))
+    polyak_test = DataLoop(G_polyak_fun, range(1))
+    polyak_test.callbacks.add_callbacks([
+        tcb.Log('imgs', 'imgs')
+    ])
+    loop = GANLoop(G, D, G_fun, D_fun, dl, device=device, log_every=100)
+    loop.G_loop.callbacks.add_callbacks([
+        tcb.Optimizer(AdamW(G.parameters(), lr=1e-4, betas=(0., 0.99))),
+    ])
+    loop.D_loop.register('G_polyak', G_polyak)
+    loop.D_loop.callbacks.add_callbacks([
+        tcb.Polyak(G, G_polyak),
+        tcb.CallDataLoop(polyak_test, prefix='polyak'),
+        tcb.Log('polyak_metrics.imgs', 'polyak_imgs'),
+        tcb.Log('batch.0', 'x'),
+        tcb.WindowedMetricAvg('real_loss'),
+        tcb.WindowedMetricAvg('fake_loss'),
+        tcb.Optimizer(AdamW(D.parameters(), lr=4e-4, betas=(0., 0.99))),
+    ])
+    loop.fit(10)
 
-            if iters % 10 == 0:
-                vis.images(fake, opts=dict(store_history=True), win='fake')
-            iters += 1
 
 if opts.shapes_only:
     summary(VggGeneratorDebug)
     summary(Patch32)
 else:
-    train_net(VggGeneratorDebug, Patch32)
+    train_net(tmodels.autogan_64, tmodels.snres_discr_4l)
