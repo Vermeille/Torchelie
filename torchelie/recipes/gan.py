943e7640169745709a2c4a0ba77b8263f93603b8
Guillaume Sanchez <gsanchez@hexaglobe.com>
2019-11-17 19:57:28 -0500

    gan recipe

---
+++ torchelie/recipes/gan.py
@@ -0,0 +1,65 @@
+import torch
+import torchelie.utils as tu
+import torchelie.metrics.callbacks as tcb
+from torchelie.recipes.recipebase import DataLoop
+
+
+def GANLoop(G,
+             D,
+             G_fun,
+             D_fun,
+             loader,
+             visdom_env='main',
+             log_every=10):
+    def D_wrap(batch):
+        tu.freeze(G)
+        G.eval()
+        tu.unfreeze(D)
+        D.train()
+
+        return D_fun(batch)
+
+    def G_wrap(batch):
+        tu.freeze(D)
+        D.eval()
+        tu.unfreeze(G)
+        G.train()
+
+        return G_fun(batch)
+
+    D_loop = DataLoop(D_wrap, loader)
+    D_loop.register('G', G)
+    D_loop.register('D', D)
+    G_loop = DataLoop(G_wrap, range(1))
+    D_loop.G_loop = G_loop
+    D_loop.register('G_loop', G_loop)
+
+    def prepare_test(state):
+        G_loop.callbacks.update_state({
+            'epoch': state['epoch'],
+            'iters': state['iters'],
+            'epoch_batch': state['epoch_batch']
+        })
+
+    D_loop.callbacks.add_prologues(
+        [tcb.Counter()])
+
+    D_loop.callbacks.add_epilogues([
+        tcb.CallDataLoop(G_loop, 1, init_fun=prepare_test, prefix='G'),
+        tcb.WindowedMetricAvg('loss'),
+        tcb.Log('G_metrics.loss', 'G_loss'),
+        tcb.Log('G_metrics.imgs', 'G_imgs'),
+        tcb.VisdomLogger(visdom_env=visdom_env,
+                         log_every=log_every),
+        tcb.StdoutLogger(log_every=log_every),
+        tcb.Checkpoint((visdom_env or 'model') + '/ckpt', D_loop)
+    ])
+
+    G_loop.callbacks.add_epilogues([
+        tcb.Log('loss', 'loss'),
+        tcb.Log('imgs', 'imgs'),
+        tcb.WindowedMetricAvg('loss')
+    ])
+
+    return D_loop
+
