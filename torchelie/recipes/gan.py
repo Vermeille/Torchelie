f9a2efdc996318bf9939abc6ec4ce5a372fb795d
Guillaume Sanchez <gsanchez@hexaglobe.com>
2020-02-19 10:31:17 -0500

    improve GAN recipe

--- torchelie/recipes/gan.py
+++ torchelie/recipes/gan.py
@@ -5,14 +5,16 @@ from torchelie.recipes.recipebase import Recipe
 
 
 def GANRecipe(G,
-             D,
-             G_fun,
-             D_fun,
-             loader,
-             *,
-             visdom_env='main',
-             checkpoint='model',
-             log_every=10):
+              D,
+              G_fun,
+              D_fun,
+              test_fun,
+              loader,
+              *,
+              visdom_env='main',
+              checkpoint='model',
+              test_every=1000,
+              log_every=10):
     def D_wrap(batch):
         tu.freeze(G)
         G.eval()
@@ -29,6 +31,14 @@ def GANRecipe(G,
 
         return G_fun(batch)
 
+    def test_wrap(batch):
+        tu.freeze(G)
+        tu.freeze(D)
+        D.eval()
+        G.eval()
+
+        return test_fun(batch)
+
     D_loop = Recipe(D_wrap, loader)
     D_loop.register('G', G)
     D_loop.register('D', D)
@@ -36,31 +46,38 @@ def GANRecipe(G,
     D_loop.G_loop = G_loop
     D_loop.register('G_loop', G_loop)
 
-    def prepare_test(state):
+    test_loop = Recipe(test_wrap, range(1))
+    D_loop.test_loop = test_loop
+    D_loop.register('test_loop', test_loop)
+
+    def G_test(state):
         G_loop.callbacks.update_state({
             'epoch': state['epoch'],
             'iters': state['iters'],
             'epoch_batch': state['epoch_batch']
         })
 
-    D_loop.callbacks.add_prologues(
-        [tcb.Counter()])
+    def prepare_test(state):
+        test_loop.callbacks.update_state({
+            'epoch': state['epoch'],
+            'iters': state['iters'],
+            'epoch_batch': state['epoch_batch']
+        })
+
+    D_loop.callbacks.add_prologues([tcb.Counter()])
 
     D_loop.callbacks.add_epilogues([
-        tcb.CallRecipe(G_loop, 1, init_fun=prepare_test, prefix='G'),
+        tcb.CallRecipe(G_loop, 1, init_fun=G_test, prefix='G'),
         tcb.WindowedMetricAvg('loss'),
         tcb.Log('G_metrics.loss', 'G_loss'),
         tcb.Log('G_metrics.imgs', 'G_imgs'),
-        tcb.VisdomLogger(visdom_env=visdom_env,
-                         log_every=log_every),
+        tcb.VisdomLogger(visdom_env=visdom_env, log_every=log_every),
         tcb.StdoutLogger(log_every=log_every),
-        tcb.Checkpoint((visdom_env or 'model') + '/ckpt_{iters}.pth', D_loop)
+        tcb.CallRecipe(test_loop,
+                       test_every,
+                       init_fun=prepare_test,
+                       prefix='Test'),
     ])
-
-    if checkpoint is not None:
-        D_loop.callbacks.add_epilogues([
-            tcb.Checkpoint(checkpoint + '/ckpt', D_loop)
-        ])
 
     G_loop.callbacks.add_epilogues([
         tcb.Log('loss', 'loss'),
@@ -68,5 +85,8 @@ def GANRecipe(G,
         tcb.WindowedMetricAvg('loss')
     ])
 
-    return D_loop
+    if checkpoint is not None:
+        test_loop.callbacks.add_epilogues(
+            [tcb.Checkpoint(checkpoint + '/ckpt_{iters}.pth', D_loop)])
 
+    return D_loop
