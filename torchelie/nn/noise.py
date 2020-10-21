97beb35825e641bb3d1ba71e9dd55566cb1c2199
Guillaume Sanchez <gsanchez@hexaglobe.com>
2020-10-21 17:43:15 -0400

    bias in Noise

--- torchelie/nn/noise.py
+++ torchelie/nn/noise.py
@@ -10,15 +10,20 @@ class Noise(nn.Module):
         ch (int): number of input channels for a different std on each channel,
             or 1
     """
-    def __init__(self, ch, inplace=False):
+    def __init__(self, ch, inplace=False, bias=None):
         super(Noise, self).__init__()
         self.a = nn.Parameter(torch.zeros(ch, 1, 1))
         self.inplace = inplace
+        self.bias = nn.Parameter(torch.zeros_like(self.a)) if bias else None
 
     def forward(self, x):
         N, C, H, W = x.shape
         z = torch.randn(N, 1, H, W, device=x.device, dtype=x.dtype)
+        z = z * self.a
+        if self.bias is not None:
+            z.add_(self.bias)
+
         if self.inplace:
-            return x.add_(z * self.a)
+            return x.add_(z)
         else:
-            return x + z * self.a
+            return x + z
