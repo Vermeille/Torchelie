878049589181e97c9351c093b6fd68abfdf85148
Guillaume Sanchez <gsanchez@hexaglobe.com>
2020-11-06 15:45:05 -0500

    more efficient standard GAN loss

--- torchelie/loss/gan/standard.py
+++ torchelie/loss/gan/standard.py
@@ -13,12 +13,12 @@ import torch.nn.functional as F
 
 
 def real(x):
-    return F.binary_cross_entropy_with_logits(x, torch.ones_like(x))
+    return F.softplus(-x).mean()
 
 
 def fake(x):
-    return F.binary_cross_entropy_with_logits(x, torch.zeros_like(x))
+    return F.softplus(x).mean()
 
 
 def generated(x):
-    return F.binary_cross_entropy_with_logits(x, torch.ones_like(x))
+    return F.softplus(-x).mean()
