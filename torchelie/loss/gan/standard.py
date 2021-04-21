321f4b34c7157dbed9251bc49c7644a94920a0cf
Guillaume Sanchez <gsanchez@hexaglobe.com>
2021-04-21 15:46:07 -0400

    reduce for standard gan loss

--- torchelie/loss/gan/standard.py
+++ torchelie/loss/gan/standard.py
@@ -12,13 +12,34 @@ import torch
 import torch.nn.functional as F
 
 
-def real(x):
-    return F.softplus(-x).mean()
+def real(x: torch.Tensor, reduce: str = 'mean') -> torch.Tensor:
+    out = F.softplus(-x)
+    if reduce == 'none':
+        return out
+    if reduce == 'mean':
+        return out.mean()
+    if reduce == 'batch_mean':
+        return out.view(out.shape[0], -1).sum(1).mean()
+    assert False, f'reduction {reduce} invalid'
 
 
-def fake(x):
-    return F.softplus(x).mean()
+def fake(x: torch.Tensor, reduce: str = 'mean') -> torch.Tensor:
+    out = F.softplus(x)
+    if reduce == 'none':
+        return out
+    if reduce == 'mean':
+        return out.mean()
+    if reduce == 'batch_mean':
+        return out.view(out.shape[0], -1).sum(1).mean()
+    assert False, f'reduction {reduce} invalid'
 
 
-def generated(x):
-    return F.softplus(-x).mean()
+def generated(x: torch.Tensor, reduce: str = 'mean') -> torch.Tensor:
+    out = F.softplus(-x)
+    if reduce == 'none':
+        return out
+    if reduce == 'mean':
+        return out.mean()
+    if reduce == 'batch_mean':
+        return out.view(out.shape[0], -1).sum(1).mean()
+    assert False, f'reduction {reduce} invalid'
