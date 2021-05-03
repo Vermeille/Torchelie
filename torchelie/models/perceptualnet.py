d70d824a59b74e90f58e227c0e4bcca1423c55c8
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2021-05-03 16:19:25 +0200

    🚨 chore(perceptualnet): disable linting on E131 to keep layers visual formatting

--- torchelie/models/perceptualnet.py
+++ torchelie/models/perceptualnet.py
@@ -27,9 +27,9 @@ class PerceptualNet(WithSavedActivations):
             'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'maxpool1',
             'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'maxpool2',
             'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3', 'relu3_3',
-                    'conv3_4', 'relu3_4', 'maxpool3',
+                    'conv3_4', 'relu3_4', 'maxpool3',  # noqa: E131
             'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3', 'relu4_3',
-                    'conv4_4', 'relu4_4', 'maxpool4',
+                    'conv4_4', 'relu4_4', 'maxpool4',  # noqa: E131
             'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3', 'relu5_3',
                     'conv5_4', 'relu5_4',  # 'maxpool5'
         ]
