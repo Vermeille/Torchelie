56512db13c71b29328dfe807ad420ba86b3467ab
Guillaume Sanchez <guillaume.v.sanchez@gmail.com>
2021-05-12 19:05:50 -0400

    VGG styles and fixes

--- torchelie/models/vgg.py
+++ torchelie/models/vgg.py
@@ -6,7 +6,18 @@ from typing import cast
 from .classifier import ClassificationHead
 
 
-class VGG(nn.Module):
+class VGG(tnn.CondSeq):
+    """
+    Construct a VGG-like model. The architecture is composed of either the
+    number of channels or 'M' for a maxpool operation.
+
+    This creates a standard VGG11 with 10 classes.
+
+    ..
+        VGG([64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+            10)
+    """
+
     def __init__(self, arch: list, num_classes: int) -> None:
         super().__init__()
         self.arch = arch
@@ -23,9 +34,8 @@ class VGG(nn.Module):
                 conv_num = 1
             else:
                 ch = cast(int, layer)
-                feats.add_module(
-                    f'conv_{block_num}_{conv_num}',
-                    tnn.ConvBlock(in_ch, ch, 3).remove_batchnorm())
+                feats.add_module(f'conv_{block_num}_{conv_num}',
+                                 tnn.ConvBlock(in_ch, ch, 3).remove_batchnorm())
                 in_ch = ch
                 conv_num += 1
         self.out_channels = ch
@@ -34,26 +44,26 @@ class VGG(nn.Module):
         self.classifier = ClassificationHead(self.out_channels, num_classes)
         self.classifier.to_vgg_style(4096)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.classifier(self.features(x))
-
-    def add_bn(self) -> 'VGG':
+    def add_batchnorm(self, remove_first=False) -> 'VGG':
         for m in self.features:
             if isinstance(m, tnn.ConvBlock):
-                m.restore_bn()
+                m.restore_batchnorm()
+
+        if remove_first:
+            self.features.conv_1_1.remove_batchnorm()
+
         return self
 
     def set_input_specs(self, in_channels: int) -> 'VGG':
         c1 = self.features.conv_1_1
         assert isinstance(c1, tnn.ConvBlock)
-        c1.conv = kaiming(tnn.Conv3x3(in_channels, c1.conv.out_channels, 3))
+        c1.conv = kaiming(tnn.Conv3x3(in_channels, c1.conv.out_channels))
         return self
 
 
 def vgg11(num_classes: int) -> 'VGG':
-    return VGG(
-        [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-        num_classes)
+    return VGG([64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+               num_classes)
 
 
 def vgg13(num_classes: int) -> 'VGG':
@@ -64,15 +74,15 @@ def vgg13(num_classes: int) -> 'VGG':
 
 def vgg16(num_classes: int) -> 'VGG':
     return VGG([
-        64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M',
-        512, 512, 512, 'M'
+        64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512,
+        512, 512, 'M'
     ], num_classes)
 
 
 def vgg19(num_classes: int) -> 'VGG':
     return VGG([
-        64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512,
-        512, 'M', 512, 512, 512, 512, 'M'
+        64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512,
+        'M', 512, 512, 512, 512, 'M'
     ], num_classes)
 
 
