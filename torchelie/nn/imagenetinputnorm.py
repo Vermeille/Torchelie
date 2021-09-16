b0118b10cfb44f0d75968180662e841cac05ee64
Guillaume "Vermeille" Sanchez <guillaume.v.sanchez@gmail.com>
2021-09-16 11:26:10 -0400

    nn.ImageNetInputNorm() works for 3D, non-batched image tensors

--- torchelie/nn/imagenetinputnorm.py
+++ torchelie/nn/imagenetinputnorm.py
@@ -11,11 +11,11 @@ class ImageNetInputNorm(nn.Module):
         super(ImageNetInputNorm, self).__init__()
         self.register_buffer(
             'norm_mean',
-            torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
+            torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1))
 
         self.register_buffer(
             'norm_std',
-            torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
+            torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1))
 
     def forward(self, input):
         return (input - self.norm_mean) / self.norm_std
