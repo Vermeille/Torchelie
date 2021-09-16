038cdf80b183317f0dd17cad4d0d0428ccf07d08
Guillaume "Vermeille" Sanchez <guillaume.v.sanchez@gmail.com>
2021-09-16 11:30:18 -0400

    PCA Lighting a la AlexNet

--- torchelie/transforms/augments.py
+++ torchelie/transforms/augments.py
@@ -26,6 +26,7 @@ __all__ = [
     'Canny',
     'SampleMixup',
     'SampleCutmix',
+    'Lighting',
 ]
 
 
@@ -266,3 +267,41 @@ class SampleCutmix:
 
     def __repr__(self):
         return 'SampleCutmix()'
+
+
+class Lighting(object):
+    """Lighting noise(AlexNet - style PCA - based noise)"""
+
+    def __init__(self, alphastd, eigval=None, eigvec=None):
+        self.alphastd = alphastd
+        self.eigval = torch.Tensor([0.2175, 0.0188, 0.0045])
+        self.eigvec = torch.Tensor([
+            [-0.5675,  0.7192,  0.4009],
+            [-0.5808, -0.0045, -0.8140],
+            [-0.5836, -0.6948,  0.4203],
+        ])
+
+        if eigval is not None:
+            self.eigval = eigval
+
+        if eigvec is not None:
+            self.eigvec = eigvec
+
+    def __call__(self, img):
+        if self.alphastd == 0:
+            return img
+
+        is_pil = False
+        if isinstance(img, PILImage):
+            img = F.to_tensor(img)
+            is_pil = True
+
+        alpha = img.new().resize_(3).normal_(0, self.alphastd)
+        rgb = self.eigvec.type_as(img).clone()\
+            .mul(alpha.view(1, 3).expand(3, 3))\
+            .mul(self.eigval.view(1, 3).expand(3, 3))\
+            .sum(1).squeeze()
+        img = img.add(rgb.view(3, 1, 1).expand_as(img)).clamp_(0, 1)
+        if is_pil:
+            img = F.to_pil_image(img)
+        return img
