dd729213cf241e5e3821091bd441986e43762029
Guillaume Sanchez <guillaume.v.sanchez@gmail.com>
2022-03-09 05:54:10 -0500

    reverse imagenetinputnorm

--- torchelie/nn/imagenetinputnorm.py
+++ torchelie/nn/imagenetinputnorm.py
@@ -7,15 +7,17 @@ class ImageNetInputNorm(nn.Module):
     Normalize images channels as torchvision models expects, in a
     differentiable way
     """
+
     def __init__(self):
         super(ImageNetInputNorm, self).__init__()
-        self.register_buffer(
-            'norm_mean',
-            torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1))
+        self.register_buffer('norm_mean',
+                             torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1))
 
-        self.register_buffer(
-            'norm_std',
-            torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1))
+        self.register_buffer('norm_std',
+                             torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1))
 
     def forward(self, input):
         return (input - self.norm_mean) / self.norm_std
+
+    def inverse(self, input):
+        return input * self.norm_std + self.norm_mean
