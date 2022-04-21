c793539a3e500ec5aa0a573f59e09f8a53de2e1b
Guillaume Sanchez <guillaume.v.sanchez@gmail.com>
2022-04-21 19:41:14 -0400

    fix padding in LocalSelfAttention

--- torchelie/nn/transformer.py
+++ torchelie/nn/transformer.py
@@ -6,6 +6,7 @@ from .conv import Conv1x1
 from .functional.transformer import local_attention_2d
 
 from typing import Optional
+from typing_extensions import Literal
 
 
 class LocalSelfAttentionHook(nn.Module):
@@ -19,17 +20,20 @@ class LocalSelfAttention2d(nn.Module):
                  num_heads: int,
                  kernel_size: int,
                  hidden_channels: Optional[int] = None,
-                 dropout=0):
+                 padding_mode: Literal['none', 'auto'] = 'none'):
         super().__init__()
-        hidden_channels = hidden_channels or in_channels
+        hidden_channels = hidden_channels or (in_channels // num_heads)
         self.hidden_channels = hidden_channels
         self.proj = tu.kaiming(
-            Conv1x1(in_channels, hidden_channels * 3, bias=False))
+            Conv1x1(in_channels, hidden_channels * num_heads * 3, bias=False))
         self.position = nn.Parameter(
             torch.zeros(num_heads, 2 * kernel_size, 2 * kernel_size))
-        self.out = tu.kaiming(Conv1x1(hidden_channels, in_channels))
+        self.out = tu.kaiming(Conv1x1(hidden_channels * num_heads,
+                                      in_channels))
         self.kernel_size = kernel_size
         self.num_heads = num_heads
+        self.attn_hook = LocalSelfAttentionHook()
+        self.padding_mode = padding_mode
 
     def unfolded_posenc(self):
         w = self.kernel_size
@@ -40,14 +44,21 @@ class LocalSelfAttention2d(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         H, W, P = x.shape[2], x.shape[3], self.kernel_size
-        pad = (P - W % P, P - H % P)
-        if pad != (0, 0):
-            x = F.pad(x, (pad[0] // 2, pad[0] - pad[0] // 2, pad[1] // 2,
-                          pad[1] - pad[1] // 2))
-        x = local_attention_2d(x, self.proj, self.unfolded_posenc(),
-                               self.num_heads, self.kernel_size)
+        pad = (-(W % -P), -(H % -P))
+        assert self.padding_mode == 'auto' or pad == (0, 0), (
+            f'kernel_size {self.kernel_size} does not divide size {W}x{H} '
+            'and padding_mode="none" specified')
+        pad = (pad[0] // 2, pad[0] - pad[0] // 2, pad[1] // 2,
+               pad[1] - pad[1] // 2)
+        if pad != (0, 0, 0, 0):
+            x = F.pad(x, pad)
+
+        x, attn = local_attention_2d(x, self.proj, self.unfolded_posenc(),
+                                     self.num_heads, self.kernel_size)
+        self.attn_hook(x, attn, pad)
+
+        if pad != (0, 0, 0, 0):
+            x = x[:, :, pad[2]:H + pad[2], pad[0]:W + pad[0]]
+
         x = self.out(x)
-        if pad == (0, 0):
-            return x
-        return x[:, :, pad[1] // 2:H + pad[1] // 2,
-                 pad[0] // 2:W + pad[0] // 2]
+        return x
